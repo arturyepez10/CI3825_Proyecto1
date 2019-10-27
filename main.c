061e1d6405c68c15eb1732eebaf3eec87ad01d3a
arturyepez10 <arturyepez10@gmail.com>
2019-10-27 16:02:45 -0400

    Documentacion adicional agregada

--- main.c
+++ main.c
@@ -51,63 +51,84 @@ int main (int argc, char **argv) {
     opterr = 0;
 
     while ((c = getopt (argc, argv, "nctf:o:z:y:v:x:s:")) != -1)
-        switch (c)
-        {
-        case 'n':
-            nflag = 1;
-            break;
-        case 'c':
-            cflag = 1;
-            break;
-        case 't':
-            tflag = 1;
-            break;
-        case 'f':
-            fvalue = optarg;
-            printf ("outarg: '%s'\n", fvalue);
-            break;
-        case 'o':
-            ovalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case 'z':
-            zvalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case 'y':
-            yvalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case 'v':
-            vvalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case 'x':
-            xvalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case 's':
-            svalue = optarg;
-            printf ("outarg: '%s'\n", optarg);
-            break;
-        case '?':
-            if (optopt == 'f' || optopt == 'o' || optopt == 'z' || optopt == 'y'|| optopt == 'v' || optopt == 'x' || optopt == 's') {
+        switch (c) {
+            /* Caso para el flag n - Ignora todo archivo o directorio que no sea regular */
+            case 'n':
+                nflag = 1;
+                break;
+            
+            /* Caso para el flag c - permite crear el mytar, si existe lo sobreescribe */
+            case 'c':
+                cflag = 1;
+                break;
+            
+            /* Caso para el flag t - muestra el contenido del mytar */
+            case 't':
+                tflag = 1;
+                break;
+
+            /* Caso para el flag f - Especifica el nombre del archivo mytar */
+            case 'f':
+                fvalue = optarg;
+                printf ("outarg: '%s'\n", fvalue);
+                break;
+            
+            /* Caso para el flag o - Directorio donde ira la salida de una extraccion */
+            case 'o':
+                ovalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+
+            /* Caso para el flag z - Cifra el contenido del archivo */
+            case 'z':
+                zvalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+
+            /* Caso para el flag y - Descifra el contenido del archivo */
+            case 'y':
+                yvalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+
+            /* Caso para el flag v - Se escribe una descripcion de lo que va haciendo */
+            case 'v':
+                vvalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+            
+            /* Caso para el flag x - Extrae todos los archivos */
+            case 'x':
+                xvalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+
+            /* Caso para el flag s - Extrae el archivo en caso de existir */
+            case 's':
+                svalue = optarg;
+                printf ("outarg: '%s'\n", optarg);
+                break;
+
+            /* Cuando no es un caso atipico */
+            case '?':
+                if (optopt == 'f' || optopt == 'o' || optopt == 'z' || optopt == 'y'|| optopt == 'v' || optopt == 'x' || optopt == 's') {
+                    /* Especifica que se necesita un argumento para ciertos flags */
                     fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-            }
-            else if (isprint (optopt)) {
-            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-            } else {
-            fprintf (stderr,
-                    "Unknown option character `\\x%x'.\n", optopt);
-            return 1;
-            }
-        default:
-            abort ();
+
+                } else if (isprint (optopt)) {
+                    /* Si no es un flag conocido */
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+
+                } else {
+                    fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+                    return 1;
+
+                }
+            default:
+                abort ();
         }
 
-    /* printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-           aflag, bflag, cvalue); */
-
+    /* Avisa en el caso que una opcion sin argumento lleva uno */
     for (index = optind; index < argc; index++)
         printf ("Non-option argument %s\n", argv[index]);
     return 0;
