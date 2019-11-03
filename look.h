724052f627e09f1ca4814ed52add66bbc6b1dedd
arturyepez10 <arturyepez10@gmail.com>
2019-11-03 01:10:10 -0400

    Arreglado orden de delcariaciones en look.h

--- look.h
+++ look.h
@@ -1,6 +1,6 @@
 #ifndef LOOK_h
 #define LOOK_h
 
-void recursive_tree(char *basePath, char *filename,const int root, int n, int v);
 int readTar(char *path, char *filename);
+void recursive_tree(char *basePath, char *filename,const int root, int n, int v);
 #endif
