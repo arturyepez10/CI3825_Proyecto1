b8d2b644e59c6f35a9c97990477169d71b0b9f96
arturyepez10 <arturyepez10@gmail.com>
2019-11-02 22:57:07 -0400

    Nuevo main de prueba

--- main.c
+++ main.c
@@ -5,10 +5,24 @@
 #include "look.h"
 
 int main(int argc, char *argv[])
-{
+{  
+    /* Se inicializa fichero que se usara para crear el archivo a empaquetar*/
     FILE *fichero;
-    fichero = fopen("./empaquetado", "w+");
+
+    /* Variable que contiene nombre de  */
+    char filename[100];
+
+    /* Recupera la respuesta del usuario */
+    printf( "Enter a value: ");
+    scanf("%s", filename);
+
+    printf( "\nYou entered: %s \n", filename);
+
+    /* Abre y crea el archivo */
+    fichero = fopen(filename, "w+");
     fclose(fichero);
-    recursive_tree(".", 1, 0, 1);
+
+    /* Empaqueta el archivo */
+    recursive_tree(".", filename, 1, 0, 1);
     return 0;
 }
